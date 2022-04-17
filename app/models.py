9396e267618b24f2c0bed877b52848842a2440f8
DevPrinceK <princesamuelpks@gmail.com>
2022-04-17 20:26:51 +0000

    update

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return self.question_text
 
     class Meta:
         db_table = 'questions'
